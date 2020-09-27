c96a40f92637494bba310b5f59224c3b14602f0f
irynailina <irynailina@gmail.com>
2020-09-27 15:41:28 +0300

    todo done

---
+++ todo-list/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda
+      explicabo tempora sint, voluptatem earum fugit et. Iusto mollitia
+      similique nemo consequuntur est sapiente blanditiis, ducimus saepe
+      asperiores nobis provident?
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
---
+++ todo-list/src/views/Todos.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <hr />
+    <AddTodo @add-todo="addTodo" />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not completed</option>
+    </select>
+    <hr />
+    <Loader v-if="loading" />
+    <TodoList
+      v-else-if="filteredTodos.length"
+      v-bind:todos="filteredTodos"
+      @remove-todo="removeTodo"
+    />
+    <p v-else>No todos!</p>
+  </div>
+</template>
+
+
+<script>
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
+import Loader from "@/components/Loader";
+export default {
+  name: "app",
+  data() {
+    return {
+      todos: [
+        // { id: 1, title: "To buy bread", completed: false },
+        // { id: 2, title: "To buy milk", completed: false },
+        // { id: 3, title: "To buy cheese", completed: false },
+      ],
+      loading: true,
+      filter: "all",
+    };
+  },
+  computed: {
+      filteredTodos () {
+          if (this.filter === 'all') {
+              return this.todos
+          } 
+          if (this.filter === 'completed') {
+              return this.todos.filter(item => item.completed)
+          }
+          if (this.filter === 'not-completed') {
+              return this.todos.filter(item => !item.completed)
+          }
+          return this.filteredTodos
+      }
+  },
+//   watch: {
+//       filter (value) {
+//           console.log(value)
+//       }
+//   },
+  components: {
+    TodoList,
+    AddTodo,
+    Loader,
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=4/")
+      .then((response) => response.json())
+      .then((json) => {
+        setTimeout(() => {
+          this.todos = json;
+          this.loading = false;
+        }, 1000);
+      });
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((item) => item.id !== id);
+    },
+
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+};
+</script>
